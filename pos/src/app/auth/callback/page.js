3f7092cbd2630aa2ac763514deafe780278f5f78
yabooung <horusr8@gmail.com>
2025-02-01 06:22:56 +0900

    feat:supa kakako 연동

--- pos/src/app/auth/callback/page.js
+++ pos/src/app/auth/callback/page.js
@@ -19,7 +19,7 @@ export default function AuthCallbackPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
-        <h2 className="text-xl mb-4">authcallback로그인 처리중...</h2>
+        <h2 className="text-xl mb-4">로그인 처리중...</h2>
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900 mx-auto"></div>
       </div>
     </div>
